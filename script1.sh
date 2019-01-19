64a82aaf11d75edda7fcda56485f10086a334230
capmisson <46821790+capmisson@users.noreply.github.com>
2019-01-19 01:06:06 +0000

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -25,5 +25,17 @@ ${SUDO} apt install curl
 ${SUDO} curl -s https://getcomposer.org/installer | php
 ${SUDO} mv composer.phar /usr/local/bin/composer
 ${SUDO} chmod +x /usr/local/bin/composer
-$echo "Edit /etc/php/7.2/fpm/pool.d/pixelfed.conf and change pixelfed username and group for your username and group"
+${SUDO} systemctl restart php7.2-fpm
+${SUDO} mkdir /var/www/vhosts
+${SUDO} mkdir /var/www/vhosts/pixelfed
+${SUDO} mkdir /var/www/vhosts/pixelfed/httpdocs
+${SUDO} mkdir /var/www/vhosts/pixelfed/logs
+${SUDO} git clone https://github.com/dansup/pixelfed.git /var/www/vhosts/pixelfed/httpdocs
+${SUDO} chown anartist:anartist /var/www/vhosts/pixelfed/httpdocs/ -R
+${SUDO} apt install zip unzip
+cd /var/www/vhosts/pixelfed/httpdocs && composer install
+cp /var/www/vhosts/pixelfed/httpdocs/.env.example /var/www/vhosts/pixelfed/httpdocs/.env
+echo "Edit /var/www/vhosts/pixelfed/httpdocs/.env"
+
+
 
